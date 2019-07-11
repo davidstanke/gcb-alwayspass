c0079ec3dd328e920530643985cdd72f073d7cf4
Dave Stanke <davidstanke@google.com>
2019-07-11 18:42:57 -0400

    Revise so mvn actually gives failing error code

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,12 +30,7 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        double r = Math.random();
-        if (r < 0.1) {
-            fail("oops");
-        } else if (r < 0.2) {
-            throw new AssumptionViolatedException("skipping");
-        }
+        fail("failing on purpose");
     }
 
 }
